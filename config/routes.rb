591f64a1601ea5d0ee72b0ae0c4176810edb6c14
Jean Aragão <aragao.jean@gmail.com>
2018-11-21 20:33:48 -0300

    Ajustes finais da aula de 09 - Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'produtos' => 'produtos#index'
-  get "/produtos/new" => "produtos#new"
-  post "/produtos" => "produtos#create"
-  delete "/produtos/:id" => "produtos#destroy", as: :produto
+  resources :produtos, only: [:new, :create, :destroy]
+    
 
   root 'produtos#index'
 end
