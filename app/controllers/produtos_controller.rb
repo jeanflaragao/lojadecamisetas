245763ec77ce7095847063600c0114459406c973
Jean Aragão <aragao.jean@gmail.com>
2018-11-27 13:22:25 -0300

    Aulas: 10 - Implementando a busca; 11 - Internacionalização da validação e 12 - Partial render convenção

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,14 +4,29 @@ class ProdutosController < ApplicationController
   end
 
   def create
-    valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
-    produto = Produto.create valores
-    redirect_to root_url
+    valores = params.require(:produto)
+      .permit :nome, :preco, :descricao, :quantidade
+    @produto = Produto.new valores
+    if @produto.save
+      flash[:notice] = "Produto salvo com sucesso."
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def destroy
     id = params[:id]
     Produto.destroy id
     redirect_to root_url
+  end
+
+  def busca
+    @nome_a_buscar = params[:nome]
+    @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+  end
+
+  def new
+    @produto = Produto.new
   end
 end
